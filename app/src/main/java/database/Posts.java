a800307e894d4f4c4d3255ddaee5a2d9b4948518
eparkhurst <eli.parkhurst@gmail.com>
2016-03-17 15:32:31 -0600

    can get data but can't parse

--- app/src/main/java/database/Posts.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-/**
- * Created by elijahparkhurst on 3/15/16.
- */
-public class Posts {
-    public Users user;
-    public String text;
-}
--- app/src/main/java/database/Users.java
+++
@@ -1,9 +0,0 @@
-package database;
-
-/**
- * Created by elijahparkhurst on 3/15/16.
- */
-public class Users {
-    public String userName;
-    public String profilePictureUrl;
-}
